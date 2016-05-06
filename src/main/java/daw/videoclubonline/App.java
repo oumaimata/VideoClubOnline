6f9df09ca17bd0225f8a5bce3895189a4aaba5d5
WieFel <felix.wielander@gmail.com>
2016-05-06 16:41:18 +0200

    Initial commit

---
+++ src/main/java/daw/videoclubonline/App.java
@@ -0,0 +1,13 @@
+package daw.videoclubonline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
